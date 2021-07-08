4fa05dce35b582ab6a6bb15cc543cd126b78e333
Matthew Lau <lau.cy.matthew@gmail.com>
2021-07-08 20:21:36 +0100

    Add components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
+  siteMetadata: {
+    title: "Foo",
+    author: "Bar",
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
